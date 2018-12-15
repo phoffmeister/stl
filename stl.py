b24a9701fc02d8b97a30e13e070285f6ea062927
pierre <pierre.git@posteo.de>
2018-12-15 01:21:11 +0100

    initial

---
+++ stl.py
@@ -0,0 +1,89 @@
+import struct
+import math
+
+
+class Vector3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return f'{self.x}, {self.y}, {self.z}'
+
+class Triangle:
+    def __init__(self, n, a, b, c):
+        self.n = n
+        self.a = a
+        self.b = b
+        self.c = c
+
+class Stl():
+    def __init__(self):
+        self.data = []
+
+    def write_stl(self, file_path):
+        print(f'Writing {len(self.data)} facets.')
+        with open(file_path, 'w') as f:
+            # write header -> 80 bytes
+            f.write('solid rock\n')
+            # write triangles n -> a -> b -> c -> 2byte 0
+            for tri in self.data:
+                f.write('facet normal %E %E %E\n' % (tri.n.x, tri.n.y, tri.n.z))
+                f.write('  outer loop\n')
+                f.write('    vertex %E %E %E\n' %(tri.a.x, tri.a.y, tri.a.z))
+                f.write('    vertex %E %E %E\n' %(tri.b.x, tri.b.y, tri.b.z))
+                f.write('    vertex %E %E %E\n' %(tri.c.x, tri.c.y, tri.c.z))
+                f.write('  endloop\n')
+                f.write('endfacet\n')
+            f.write('endsolid rock')
+
+    def write_stl_bin(self, file_path):
+        with open(file_path, 'wb') as f:
+            # write header -> 80 bytes
+            f.write(b'a'*80)
+            # write number of triangles -> uint32 -> (4byte)
+            f.write(struct.pack('<i', len(self.data)))
+            # write triangles n -> a -> b -> c -> 2byte 0
+            for tri in self.data:
+                f.write(struct.pack('<3f', tri.n.x, tri.n.y, tri.n.z))
+                f.write(struct.pack('<3f', tri.a.x, tri.a.y, tri.a.z))
+                f.write(struct.pack('<3f', tri.b.x, tri.b.y, tri.b.z))
+                f.write(struct.pack('<3f', tri.c.x, tri.c.y, tri.c.z))
+                f.write(struct.pack('\x00'*2))
+
+class Cylinder(Stl):
+    def __init__(self, r, h, facets):
+        super().__init__()
+        n = Vector3(0, 0, 0)
+        c1 = self._get_circle(r, 0, facets)
+        c1_center = Vector3(0, 0, 0)
+        c2 = self._get_circle(r, h, facets)
+        c2_center = Vector3(0, 0, h)
+
+        for p in range(len(c1)-1):
+            self.data.append(Triangle(n, c1[p], c1[p+1], c1_center))
+        self.data.append(Triangle(n, c1[0], c1[-1], c1_center))
+
+        for p in range(len(c2)-1):
+            self.data.append(Triangle(n, c2[p], c2[p+1], c2_center))
+        self.data.append(Triangle(n, c2[0], c2[-1], c2_center))
+
+        for p in range(len(c1)-1):
+            self.data.append(Triangle(n, c1[p], c1[p+1], c2[p]))
+            self.data.append(Triangle(n, c1[p+1], c2[p], c2[p+1]))
+        self.data.append(Triangle(n, c1[-1], c1[0], c2[-1]))
+        self.data.append(Triangle(n, c1[0], c2[0], c2[-1]))
+
+    def _get_circle(self, r, z, amount):
+        circle = []
+        deg = float(360.0 / amount)
+        for n in range(amount):
+            x = r * math.sin( math.radians(n * deg))
+            y = r * math.cos( math.radians(n * deg))
+            circle.append(Vector3(x, y, z))
+        return circle
+
+if __name__ == '__main__':
+    mstl = Cylinder(10, 50, 300)
+    mstl.write_stl("cyl.stl")
